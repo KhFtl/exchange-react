7fd106228cb60345661e55365a0023e8093f2a6d
Віталій Шовкун <vetalshovkun@gmail.com>
2026-06-01 20:02:54 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  output: {
+    assetPrefix: "https://khftl.github.io/exchange-react",
+  },
 })
